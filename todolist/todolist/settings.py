f9605540d5a7343bfff7524b5fb1c3dbc5d0e58b
Maksym Tymoshchenko <maksym.tymoshchenko@outlook.com>
2021-06-03 19:56:19 +0300

    GeekBrains: Authentication (JWT), Permissions

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -161,5 +161,10 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 100,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    )
 }
